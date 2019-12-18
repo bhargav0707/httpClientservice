28a6b480e993ad6625d4534863101abdcd940b8d
Kottala Bhargav Kumar <kottala.bhargav77@gmail.com>
2019-12-18 12:33:53 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { EmployeeCreateComponent } from './employee-create/employee-create.component';
 import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { UrikeComponent } from './urike/urike.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeCreateComponent,
     EmployeeEditComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    UrikeComponent
   ],
   imports: [
     BrowserModule,
